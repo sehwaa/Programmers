34335f77354486fc447d506e7427c2c9bdb55ac6
sehwaa <nsh235482@gmail.com>
2020-08-25 22:17:17 +0900

    테스트케이스 1개 미통과

---
+++ heap/더_맵게.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<int, vector<int>, greater<int> > pq;
+    int min = 0;
+    int secondMin = 0;
+    int answer = 0;
+    
+    for (int i = 0; i < scoville.size(); i++) {
+        pq.push(scoville.at(i));
+    }
+    
+    if (pq.top() >= K) return -1;
+    while (!pq.empty()) {
+        answer++;
+        
+        if (pq.size() <= 1) {
+            answer = 0;
+            break;
+        }
+        
+        min = pq.top();
+        pq.pop();
+        secondMin = pq.top();
+        pq.pop();
+        
+        int newScoville = min + (2 * secondMin);
+        
+        if (pq.top() >= K && newScoville >= pq.top()) break;
+        else pq.push(newScoville);
+    }
+    
+    if (answer == 0) answer = -1;
+    
+    return answer;
+}
