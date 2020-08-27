bbb13594c9a8a8ae95b471e19da069eee89f3c24
sehwaa <nsh235482@gmail.com>
2020-08-28 00:51:42 +0900

    큐가 중간에 비게 될 경우 예외처리 안됨

--- C++/heap/이중우선순위큐.cpp
+++ C++/heap/이중우선순위큐.cpp
@@ -7,27 +7,39 @@ using namespace std;
 
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    priority_queue< int, vector<int> > g_pq;
-    priority_queue< int, vector<int> > l_pq;
+    int max_pivot = 0;
+    int min_pivot = 0;
+    priority_queue< int, vector<int> > pq;
     
     for (int i = 0; i < operations.size(); i++) {
-       if (operations.at(i) == "D 1" && g_pq.size() > 1) {
-           g_pq.pop();
-       } else if (operations.at(i) == "D -1" && l_pq.size() > 1) {
-           l_pq.pop();
-       } else {
-           g_pq.push(atoi(operations.at(i).replace(0,2,"").c_str()));
-           l_pq.push(atoi(operations.at(i).replace(0,2,"").c_str()));
-       }
+        if (operations.at(i) == "D 1") {
+            max_pivot++;
+        } else if (operations.at(i) == "D -1") {
+            min_pivot++;
+        } else {
+            pq.push(atoi(operations.at(i).replace(0,2,"").c_str()));
+        }
+    }
+
+    int max;
+    for (int i = 0; i < max_pivot; i++) {
+        if (!pq.empty()) {
+            max = pq.top();
+            pq.pop();
+        }
     }
     
-    // if (pq.size() > 0) {
-    //     answer.push_back(g_pq.top());
-    //     answer.push_back(l_pq.top());
-    // } else {
-    //     answer.push_back(0);
-    //     answer.push_back(0);
-    // }
+    int min;
+    int left_size = pq.size() - min_pivot;
+    for (int i = 0; i < left_size; i++) {
+        if (!pq.empty()) {
+            min = pq.top();
+            pq.pop();
+        }
+    }
+    
+    answer.push_back(max);
+    answer.push_back(min);
     
     return answer;
 }
