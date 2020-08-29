23581c33595176fd74d799d80d6066274e82829f
sehwaa <nsh235482@gmail.com>
2020-08-29 21:13:19 +0900

    PASS

--- C++/heap/이중우선순위큐.cpp
+++ C++/heap/이중우선순위큐.cpp
@@ -9,7 +9,7 @@ using namespace std;
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
     deque<int> q;
-    
+
     for (int i = 0; i < operations.size(); i++) {
         if (operations.at(i) == "D 1" && q.size() > 0) {
             sort(q.begin(), q.end());
@@ -21,12 +21,13 @@ vector<int> solution(vector<string> operations) {
             q.push_back(stoi(operations.at(i).substr(2)));
         }
     }
-    
+
     if (q.empty()) {
         answer.push_back(0);
         answer.push_back(0);
         return answer;
     } else {
+        sort(q.begin(), q.end());
         answer.push_back(q.back());
         answer.push_back(q.front());
         return answer;
