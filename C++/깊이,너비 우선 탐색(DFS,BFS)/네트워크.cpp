f41e2ef3ce98a428709b8d8e26e45cb5f07c101e
Sarah Na <sehwaa@users.noreply.github.com>
2020-11-05 08:03:16 +0900

    예제 테스트케이스 통과

--- C++/깊이,너비 우선 탐색(DFS,BFS)/네트워크.cpp
+++ C++/깊이,너비 우선 탐색(DFS,BFS)/네트워크.cpp
@@ -8,15 +8,21 @@ int answer = 0;
 int allComputers[MAX][MAX];
 bool visited[MAX][MAX];
 int ComputerCnt = 0;
+int count = 0;
 
 void dfs(int index) {
+    count++;
     for (int i = 0; i < ComputerCnt; i++) {
         if (i == index) continue;
-        if (!visited[index][i] &&  allComputers[index][i] == 1) {
-            visited[index][i] = true;
-            visited[i][index] = true;
-            answer++;
-            dfs(i);
+        if (allComputers[index][i] == 1) {
+            if (!visited[index][i]) {
+               visited[index][i] = true;
+               visited[i][index] = true; 
+                count++;
+                dfs(i);
+            } else {
+                count -= 1;
+            }
         }
     }
 }
@@ -32,6 +38,10 @@ int solution(int n, vector<vector<int>> computers) {
     
     for (int i = 0; i < n; i++) {
         dfs(i);
+        if (count >= 1) {
+            count = 0;
+            answer++;
+        }
     }
     return answer;
 }
