452214503f05931ec1dade5f81f335417ede592e
Sarah Na <nsh235482@gmail.com>
2021-08-20 11:43:23 +0900

    game board 회전 조각 까지 알아냄

--- Python/위클리3주차.py
+++ Python/위클리3주차.py
@@ -6,8 +6,16 @@ blocks = defaultdict(list)
 visited = []
 
 
-def make_rotation(x, y):
-    pass
+def make_rotation(index, count, block_list):
+    global blocks
+
+    rotate = [[-arr[1], arr[0]] for arr in block_list]
+
+    if rotate == blocks[index][0] and count is not 0:
+        return
+
+    blocks[index].append(rotate)
+    make_rotation(index, count+1, rotate)
 
 
 def search(x, y, count, game_board):
@@ -17,10 +25,10 @@ def search(x, y, count, game_board):
     for i in range(0, 4):
         px = x + direction_x[i]
         py = y + direction_y[i]
-        if px >= 0 and py >= 0 and px < len(game_board) and py < len(game_board) and game_board[px][py] == 0 and \
-                visited[px][py] == False:
+        if 0 <= px < len(game_board) and 0 <= py < len(game_board) and game_board[px][py] == 0 and \
+                visited[px][py] is False:
             arr = search(px, py, count, game_board)
-            blocks[count].append(arr)
+            blocks[count][0].append(arr)
 
     return [x, y]
 
@@ -37,10 +45,23 @@ def solution(game_board, table):
     for x_idx, arr in enumerate(game_board):
         for y_idx, value in enumerate(arr):
             if value == 0 and visited[x_idx][y_idx] == False:
+                blocks[count].append([])
                 arr = search(x_idx, y_idx, count, game_board)
-                blocks[count].append(arr)
+                blocks[count][0].append(arr)
                 count += 1
 
-    print(blocks)
+    for k in blocks.keys():
+        make_rotation(k, 0, blocks[k][0])
+        print("k: ", k)
+        print(blocks[k])
+
     answer = -1
     return answer
+
+
+answer = solution([[1, 1, 0, 0, 1, 0], [0, 0, 1, 0, 1, 0], [0, 1, 1, 0, 0, 1], [1, 1, 0, 1, 1, 1], [1, 0, 0, 0, 1, 0],
+          [0, 1, 1, 1, 0, 0]],
+         [[1, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 0], [0, 1, 1, 0, 1, 1], [0, 0, 1, 0, 0, 0], [1, 1, 0, 1, 1, 0],
+          [0, 1, 0, 0, 0, 0]])
+
+print(answer)
