3f6d38ef385342a00d8a6d3d5b46a91fc7878315
Sarah Na <nsh235482@gmail.com>
2021-10-26 22:24:40 +0900

    10번 시간초과 해결

--- Python/Greedy-큰수만들기.py
+++ Python/Greedy-큰수만들기.py
@@ -1,13 +1,14 @@
 def solution(number, k):
-    number = list(number)
     su = []
     
+    i = 0
     while k > 0 and len(number) > 0:
-        while len(su) > 0 and su[-1] < number[0]:
+        while len(su) > 0 and su[-1] < number[i]:
             su.pop()
             k -= 1
             if k == 0:
                 break
-        su.append(number.pop(0))
+        su.append(number[i])
+        i += 1
 
-    return "".join(su+number)
+    return "".join(su) + number[i:]
